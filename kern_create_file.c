7224fbe1011df0705d20205081707794e07fd93b
Ankit <ankitgupta.work@gmail.com>
2013-04-18 11:25:57 +0530

    kern_create_file: Debug level changed in printk, module description,     author and License added.

--- kern_create_file.c
+++ kern_create_file.c
@@ -11,10 +11,10 @@
 struct file *filp;
 static int __init add(void)
 {
-	printk("%s\n", __func__);
+	printk(KERN_DEBUG"%s\n", __func__);
 	filp=filp_open("/home/kopal/git/linux-kernel-modules/kern_file",O_CREAT,0);
 	if (IS_ERR(filp)) {
-		printk(KERN_WARNING "**Error**\n");	
+		printk(KERN_ERR"**Error**\n");	
 		return -1;
 	}
 	return 0;
@@ -23,9 +23,12 @@ static int __init add(void)
 static void __exit remove(void)
 {
 	filp_close(filp,NULL);
-	printk("%s\n", __func__);
+	printk(KERN_DEBUG"%s\n", __func__);
 	
 } 
 module_init(add);
 module_exit(remove);
 
+MODULE_DESCRIPTION("Create File in UserLand");
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kopal <kopalsingh.work@gmail.com>");
